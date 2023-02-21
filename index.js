5dfb2e11140a32a7928a2ca1f284e8ae1bb9f3e8
ROHAN <ro.anilpnr@gmail.com>
2023-02-21 19:30:10 +0530

    backend skeleton

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// require("./startup/routes")(app);
+// require("./startup/logging")();
+// require("./startup/config")();
+// require("./startup/validation")();
+// require("./startup/db")();
+// require("./startup/prod")(app);
+
+const server = app.listen(3000, () => console.log("Connected to port 3000"));
+module.exports = server;
