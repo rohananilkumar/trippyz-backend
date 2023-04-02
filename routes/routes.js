d502dfc0df4d735b8278c780703f0b67ad67ec70
ROHAN <ro.anilpnr@gmail.com>
2023-04-03 03:05:46 +0530

    GCP integration complete

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,8 @@ const {
   routeRequestMongooseSchema,
   RouteRequest,
 } = require("../models/routerequest");
+const config = require("config");
+const fetch = require("node-fetch");
 
 router.get("/", async (req, res) => {
   console.log("get request");
@@ -25,6 +27,50 @@ router.post("/get-route", auth, async (req, res) => {
   const result = await route.save();
 
   return res.status(200).send({ message: "success", value: result });
+});
+
+router.get("/lat-long/:area", auth, async (req, res) => {
+  const area = req.params.area;
+
+  const key = config.get("gmap");
+  console.log(area, key);
+
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${area}&key=${key}`
+  );
+
+  const body = await response.json();
+
+  // const coordinates = body.feature;s[0].geometry
+
+  return res.status(200).send(body.results[0].geometry);
+});
+
+router.get("/tourist-places/:area", auth, async (req, res) => {
+  const area = req.params.area;
+
+  const key = config.get("gmap");
+
+  const coordinateResponse = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${area}&key=${key}`
+  );
+
+  const coordinatesBody = await coordinateResponse.json();
+
+  const coordinates = coordinatesBody.results[0].geometry.location;
+  console.log(coordinates);
+  const radius = 3000;
+
+  //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radius}&type={type}&key={API_KEY}
+  const type = "tourist_attraction";
+  const requeststring = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates.lat},${coordinates.lng}&radius=${radius}&type=${type}&key=${key}`;
+  const tourismResponse = await fetch(requeststring);
+  const touristPlaces = await tourismResponse.json();
+
+  return res.status(200).send({
+    coordinates,
+    touristPlaces,
+  });
 });
 
 module.exports = router;
